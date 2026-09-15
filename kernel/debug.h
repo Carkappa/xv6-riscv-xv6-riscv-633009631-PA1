86c73e84d8d5338da50a1f62d0ed4d5d76928ad3
Carkappa <car1kappa123@tamu.edu>
2026-09-15 08:51:37 -0500

    Editing the syntax and adding unsigned external values for categories

--- kernel/debug.h
+++ kernel/debug.h
@@ -1,3 +1,6 @@
+#ifndef DEBUG_H
+#define DEBUG_H
+
 #define DBG_PROC (1UL << 0) /* process lifecycle */
 #define DBG_SCHED (1UL << 1) /* context switching */
 #define DBG_SYSCALL (1UL << 2) /* system call entry/exit */
@@ -6,3 +9,15 @@
 #define DBG_FS (1UL << 5) /* file system */
 #define DBG_DEV (1UL << 6) /* drivers */
 #define DBG_ALL (~0UL)
+extern unsigned long debug_mask;
+
+
+#define DBG_ERR 1
+#define DBG_WARN 2
+#define DBG_INFO 3
+#define DBG_TRACE 4
+extern int debug_level;
+
+#endif /* DEBUG_H */
+
+
